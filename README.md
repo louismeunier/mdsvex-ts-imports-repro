6ec915d21c93a1c88c3d966f2786989fecbe9a12
louismeunier <louismeunier123@gmail.com>
2022-02-03 19:31:19 -0500

    Fixed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+## Now fixed
+
+see [vite config](/vite.config.js) for fix
+
 repro for https://github.com/pngwn/MDsveX/issues/419
 
 relevant issue [here](/src/lib/Test.svx)
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tsconfigPaths()]
+  plugins: [svelte(), tsconfigPaths({
+    loose: true
+  })]
 })
